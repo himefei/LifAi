eac6eaa259225a36ababb0aadc686cd4642a14cc
Lifei Yu <hime.fei@gmail.com>
2024-11-01 22:27:21 +1100

    remove venv and other wrongly uploaded folder

--- old/improvement_options.py
+++
@@ -1,9 +0,0 @@
-improvement_options = [
-    "Fix spelling",
-    "Improve writing quality",
-    "Make text more polite and friendly",
-    "Make it simple and easy to read",
-    "Summarize context",
-    "Analyze context and generate response",
-    "Translate to Simplified Chinese"
-]
